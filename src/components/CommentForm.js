6eabe84fc864bc4f6c576ae4aed30a1f3bb2cbdb
Marc Rayner <26255479+marcjrayner@users.noreply.github.com>
2020-01-28 10:29:58 +0000

    implements handling state change in commentform

--- src/components/CommentForm.js
+++ src/components/CommentForm.js
@@ -1,14 +1,42 @@
 import React, {Component} from 'react';
 
 class CommentForm extends Component{
+  constructor(props) {
+    super(props);
+    this.state = {
+      author: "",
+      text: ""
+    }
+    this.handleAuthorChange = this.handleAuthorChange.bind(this); // CONTEXT = constructor and render cannot see each other at all, in order for the methods in the form, we need to pass it into the constructor, bind them into the constructor in order for them to work.
+    this.handleTextChange = this.handleTextChange.bind(this);
+  }
+
+  handleAuthorChange(e) {
+    this.setState({author: e.target.value})
+  }
+
+  handleTextChange(e) {
+    this.setState({text: e.target.value})
+  }
+
   render() {
     return(
       <form className='comment-form'>
-        <input type="text" placeholder="Author name"/>
-        <input type="text" placeholder="comment"/>
+        <input
+          type="text"
+          placeholder="Author name"
+          value={this.state.author}
+          onChange={this.handleAuthorChange}
+        />
+        <input
+          type="text"
+          placeholder="comment"
+          value={this.state.text}
+          onChange={this.handleTextChange}
+        />
         <input type="submit" value="Add Comment"/>
       </form>
-    ;
+    );
   }
 }
 export default CommentForm
--- src/containers/CommentBox.js
+++ src/containers/CommentBox.js
@@ -1,5 +1,7 @@
 import React, {Component} from 'react';
 import CommentList from '../components/CommentList';
+import CommentForm from '../components/CommentForm'
+
 
 class CommentBox extends Component {
 
@@ -7,17 +9,17 @@ class CommentBox extends Component {
     super(props);
     this.state = {
       data: [
-        { 
+        {
           id: 1,
           author: "John",
           text: "I love JS more each day."
         },
-        { 
+        {
           id: 2,
           author: "Juan",
           text: "Just wait until we add the form. It's gonna be so good."
         }
-      ] 
+      ]
     }
   }
 
@@ -25,6 +27,8 @@ class CommentBox extends Component {
   render() {
     return (
       <div className="comment-box">
+        <h2>Add a comment</h2>
+        <CommentForm/>
         <h2>Comments</h2>
           <CommentList data={this.state.data}/>
       </div>
